d27caf76999dfe05d0162b8e2ba58b16629474f9
Gyaneshwar Kumar <codewithgyan@gmail.com>
2022-02-23 23:01:10 +0530

    added authentication to backend

--- server/src/models/users.model.js
+++ server/src/models/users.model.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
-
+const bcrypt=require('bcrypt');
 const userSchema = new mongoose.Schema(
   {
-    firstName: { type: String },
-    lastName: { type: String },
-    displayName: { type: String },
-    email: { type: String, required: true },
+    firstName: { type: String,required:true},
+    lastName: { type: String,required:true},
+    displayName: { type: String,required:false},
+    email: { type: String, required: true,unique:true},
+    password:{type:String,required:true},
     orders: [
       { type: mongoose.Schema.Types.ObjectId, ref: "games", required: false },
     ],
@@ -18,5 +19,15 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-
+userSchema.methods.checkPassword=function(password)
+{
+   const hashedPassword=this.password;
+   return bcrypt.compareSync(password, this.password);
+}
+userSchema.pre("save",function(next){
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hashSync(this.password, 8);
+    return next();
+        
+});
 module.exports = mongoose.model("user", userSchema);

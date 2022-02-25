37d89195f8e6e9bf680a46c60cae0443ead5b0ce
Imran Khan <imrankhmd@gmail.com>
2022-02-25 15:29:46 +0530

    Imran01

---
+++ epic-clone/src/App.jsx
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header.jsx";
+
+import Footer from './components/Footer/Footer';
+import { Navbar } from './components/Navbar';
+
+
+
+
+
+function App() {
+  return (<div className="App" >
+    <Header />
+    <Navbar />
+    <Footer />
+
+  </div>
+  );
+}
+
+export default App;

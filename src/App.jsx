56510784ccb1d57bba089fa332641a36cde06961
Gyaneshwar Kumar <codewithgyan@gmail.com>
2022-02-27 00:48:02 +0530

    .

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { MainSlider } from './components/MainSlider';
+import { Browse } from './components/Browse/Browse';
+
+function App() {
+  return (
+    <div className="App">
+        {/* <MainSlider/> */}
+        <Browse/>
+    </div>
+  );
+}
+
+export default App;

415f5a068838279cfef126719916301555dc2027
Gyaneshwar Kumar <codewithgyan@gmail.com>
2022-02-27 12:25:55 +0530

    login and signup completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { MainSlider } from './components/MainSlider';
 import { Browse } from './components/Browse/Browse';
-
+import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
 function App() {
   return (
     <div className="App">
         {/* <MainSlider/> */}
-        <Browse/>
+        {/* <Browse/> */}
+        <Login/>
+        {/* <SignUp/> */}
     </div>
   );
 }
